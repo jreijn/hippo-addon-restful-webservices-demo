5aba53f65f7c5335aa5b381d0504ed04c09ed102
Jeroen Reijn <j.reijn@gmail.com>
2014-11-21 09:46:57 +0100

    Added some example scripts on how to work with the API

---
+++ scripts/find-five-documents.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+curl --user admin:admin -H "Content-Type: application/json" -XPOST http://localhost:8080/cms/rest/api/_query/ -d '
+{
+  "statement":"/jcr:root/content/documents//element(*,hippo:document)",
+  "language":"xpath",
+  "limit":5
+}'

72c505859354bd5afbd7cf967e1eeed8778a5cf2
Jeroen Reijn <j.reijn@gmail.com>
2015-02-23 21:11:58 +0100

    Bump project to version 0.3.0 of the REST API plugin Updated scripts Cleaned up web.xml

--- scripts/find-five-documents.sh
+++ scripts/find-five-documents.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl --user admin:admin -H "Content-Type: application/json" -XPOST http://localhost:8080/cms/rest/api/_query/ -d '
+curl --user admin:admin -H "Content-Type: application/json" -XPOST http://localhost:8080/cms/rest/api/v1/_query/ -d '
 {
   "statement":"/jcr:root/content/documents//element(*,hippo:document)",
   "language":"xpath",
---
+++ scripts/list-users.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -v --user admin:admin -H "Content-Type: application/json" -XGET http://localhost:8080/cms/rest/api/v1/users

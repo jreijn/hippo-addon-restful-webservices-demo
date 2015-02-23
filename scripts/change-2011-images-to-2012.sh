e2d7f7d115da6206828e1c142706c62e904cb50e
Jeroen Reijn <j.reijn@gmail.com>
2015-02-23 22:44:32 +0100

    Bump project to version 0.3.1 of the REST API plugin Updated scripts

--- scripts/change-2011-images-to-2012.sh
+++ scripts/change-2011-images-to-2012.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-curl -XGET --user admin:admin -H "Content-Type: application/json" http://localhost:8080/cms/rest/api/v1/nodes/content/gallery/myhippoproject/news/2011/?depth=5 > news-2011.json
+curl -XGET --user admin:admin -H "Content-Type: application/json" http://localhost:8080/cms/rest/api/nodes/content/gallery/myhippoproject/news/2011/?depth=5 > news-2011.json
 sed -i.bak s/2011/2012/g news-2011.json
-curl -XPUT --user admin:admin -H "Content-Type: application/json" -d@news-2011.json http://localhost:8080/cms/rest/api/v1/nodes/content/gallery/myhippoproject/news/2011
+curl -XPUT --user admin:admin -H "Content-Type: application/json" -d@news-2011.json http://localhost:8080/cms/rest/api/nodes/content/gallery/myhippoproject/news/2011
--- scripts/list-users.sh
+++ scripts/list-users.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -v --user admin:admin -H "Content-Type: application/json" -XGET http://localhost:8080/cms/rest/api/v1/users
+curl -v --user admin:admin -H "Content-Type: application/json" -XGET http://localhost:8080/cms/rest/api/users
